c99c2b41d61dbab9be37943063d805567f73ccef
luxe0x64 <123239129+luxe0x64@users.noreply.github.com>
2025-03-12 17:15:35 +0100

    New version 1.5
    I fixed a bug with function. Added main function. Have a nice day. Thank you for waiting.

---
+++ nbconverter.py
@@ -0,0 +1,61 @@
+import os
+from moviepy import *
+from time import sleep
+
+###################################################################################
+#                           Made by: nobody3132 aka luxe0x64                      #
+#                               Date: 14 February 2025                            #
+#                               Happy Vilentine's day                             #
+#                                    Version: 1.5                                 #
+###################################################################################
+
+
+
+class Convert:
+    def __init__(self):
+        os.system('clear')
+        os.system('cat nbconverver_banner.txt')
+        self.name_of_file_to_convert = None
+        self.converted_file_name = None
+        self.format = None
+    pass
+
+
+    def Formats(self):
+        print("supported formats: mp3 mp4 m4a avi etc.")
+        self.format = input("Enter file format TO convert: ")
+        print(f"Format selected: {self.format}")
+        sleep(3)
+        os.system("clear")
+        os.system('cat nbconverver_banner.txt')
+        os.system('ls')
+    pass
+
+    def Converting(self):
+        self.name_of_file_to_convert = input("Enter the name of file with extensions example: music.mp3\nEnter the name of file to convert: ")
+        try:
+            os.system('ls | grep ' + self.name_of_file_to_convert)
+            print("Found. ")
+            print("Name of the file to convert: " + self.name_of_file_to_convert)
+            self.converted_file_name = input("Enter converted file name, example: music.m4a\nEnter converted file name: ")
+            print("Converting... ")
+            try:
+                video = VideoFileClip(os.path.join(self.name_of_file_to_convert))
+                video.audio.write_audiofile(os.path.join(self.converted_file_name))
+                print("Converted. " + "File saved as: " + self.converted_file_name)
+            except Exception as e:
+                print("Something went wrong.... ")
+                print(e)
+        except Exception as e:
+            print("File not found. ")
+            print(e)
+    pass
+
+
+    def main(self):
+        self.Formats()
+        self.Converting()
+    pass
+
+ConverProgram = Convert()
+ConverProgram.main()
